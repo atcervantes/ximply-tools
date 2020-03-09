bc9a7a105a894190e85e19709654c9ad9cfcb974
atcervantes <inge.cervantes@gmail.com>
2020-03-09 16:44:26 +0100

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    employees:[
+        {name: "Angel Cervantes"},
+        /*{name: "Dan Hughes"},
+        {name: "Diego"},
+        {name: "Markus"},
+        {name: "Pablo Martinez"},
+        {name: "Edmar Dos Reis"}*/
+]};
